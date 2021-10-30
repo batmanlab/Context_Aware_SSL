5ecbe1e27a68c00fff11afd3a0edc1dbbc1fabf0
Li Sun <lisun97@foxmail.com>
2021-10-29 23:35:04 -0400

    Update dataset_train_graph.py

--- dataset_train_graph.py
+++ dataset_train_graph.py
@@ -11,31 +11,11 @@ class COPD_dataset(Dataset):
     def __init__(self, stage, cfg, transforms=default_transform, graph_cutoff=0.13):
         self.cfg = cfg
         self.root_dir = cfg.root_dir
-        self.metric_dict = dict() # initialize metric dictionary
         self.transforms = transforms
         self.graph_cutoff = graph_cutoff # hyper-parameter used to control graph sparsity
-        
-        # Read feature database for evaluation purpose only, replace it with your database or remove it
-        FILE = open("/pghbio/dbmi/batmanlab/Data/COPDGene/ClinicalData/phase 1 Final 10K/phase 1 Pheno/Final10000_Phase1_Rev_28oct16.txt", "r")
-        mylist = FILE.readline().strip("\n").split("\t")
-        metric_idx = [mylist.index(label) for label in self.cfg.label_name]
-        race_idx = mylist.index("race")
-        for line in FILE.readlines():
-            mylist = line.strip("\n").split("\t")
-            tmp = [mylist[idx] for idx in metric_idx]
-            if "" in tmp:
-                continue
-            metric_list = []
-            for i in range(len(metric_idx)):
-                metric_list.append(float(tmp[i]))
-            self.metric_dict[mylist[0]] = metric_list
-        FILE.close()
 
-        # Filter images that doesn't have associated features
         self.sid_list = []
         for item in glob.glob(self.cfg.root_dir+"patch/"+"*_patch.npy"):
-            if item.split('/')[-1][:6] not in self.metric_dict:
-                continue
             self.sid_list.append(item.split('/')[-1][:-10])
         self.sid_list.sort()
         self.patch_loc = np.load("/pghbio/dbmi/batmanlab/lisun/copd/gnn_shared/data/patch_data_32_6_reg/19676E_INSP_STD_JHU_COPD_BSpline_Iso1_patch_loc.npy")
